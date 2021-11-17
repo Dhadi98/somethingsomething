5f4023686b0f6ffd29a3af9c8d00750ce862b51a
Dhadi98 <94518490+Dhadi98@users.noreply.github.com>
2021-11-17 02:13:21 -0500

    Create Header_For_All.php

---
+++ Header_For_All.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content= "width=device-width, user-scalable=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+    
+    
+    </head>
+    <body> 
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+            <div class="input-group mb-5">
+                <div class="input-group-prepend">
+                   
+            </div>
+<div class="container table-responsive">
+            </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    </body>
+    </html>
